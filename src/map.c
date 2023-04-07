ca00f11fed693ee060a264f605c00b5319edd854
HolyPite <120518703+HolyPite@users.noreply.github.com>
2023-04-07 14:40:06 +0200

    Update map.c

--- src/map.c
+++ src/map.c
@@ -28,8 +28,22 @@ Color get_map_value (Map* map, int x, int y){
 	return map -> map[y * map -> size + x];
 }
 
-void fill_map(Map* map){
+void fill_map(Map* map) {
+    // Parcourir chaque cellule de la carte
+    for (int y = 0; y < map->size; y++) {
+        for (int x = 0; x < map->size; x++) {
+            // Générer un nombre aléatoire entre 0 et 6 et affecter la couleur correspondante à la cellule de la carte aux coordonnées (x, y)
+            set_map_value(map, x, y, rand() % 7);
+        }
+    }
+
+    // Positionner le joueur 1 en bas à gauche de la carte
+    set_map_value(map, 0, map->size - 1, PLAYER1);
+    
+    // Positionner le joueur 2 en haut à droite de la carte
+    set_map_value(map, map->size - 1, 0, PLAYER2);
 }
+
 int main(int argc, char** argv){
 srand(time(NULL));
 }
